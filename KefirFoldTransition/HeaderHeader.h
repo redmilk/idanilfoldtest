ffbdcc914da31f56545baed0fa7aee57c599f3ac
Artem <Artem@Danils-MacBook-Pro.local>
2017-02-24 11:46:10 +0200

    fold init state

---
+++ KefirFoldTransition/HeaderHeader.h
@@ -0,0 +1,16 @@
+//
+//  HeaderHeader.h
+//  KefirFoldTransition
+//
+//  Created by Artem on 2/24/17.
+//  Copyright © 2017 ApiqA. All rights reserved.
+//
+
+#ifndef HeaderHeader_h
+#define HeaderHeader_h
+
+
+#endif /* HeaderHeader_h */
+
+#import "MPFoldTransition.h"
+
